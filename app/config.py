8186740058a8ec54b1ea8b8cde5c83cbbb245c15
Eric Cao <ericcao561@gmail.com>
2026-03-16 18:38:54 -0500

    added square to dependencies and config

--- app/config.py
+++ app/config.py
@@ -8,3 +8,7 @@ class Config:
     TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
     TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
     TWILIO_PHONE = os.getenv("TWILIO_PHONE")
+
+    SQUARE_ACCESS_TOKEN = os.getenv("SQUARE_ACCESS_TOKEN")
+    SQUARE_ENV = os.getenv("SQUARE_ENV", "sandbox")
+    SQUARE_VERSION = os.getenv("SQUARE_VERSION", "2026-01-22")
