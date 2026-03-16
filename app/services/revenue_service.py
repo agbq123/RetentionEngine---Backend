4c3429e68a2beb191f36b1e1d8251f1a90e09497
agbq123 <abdulghanibadrquraishi@gmail.com>
2026-03-15 20:34:32 -0500

    Improved recovery val func from skeletion to sumn workable

--- app/services/revenue_service.py
+++ app/services/revenue_service.py
@@ -1,6 +1,11 @@
 def estimate_recovery_value(client):
-    if client.visit_count == 0:
+    if client.visit_count < 2:
         return 0
 
     avg_value = client.lifetime_value / client.visit_count
-    return avg_value * 6  # assume 6 future visits recovered
+
+    weeks_active = (client.last_visit - client.first_visit).days / 7
+
+    visits_per_year = client.visit_count / (weeks_active / 52)
+
+    return avg_value * visits_per_year
