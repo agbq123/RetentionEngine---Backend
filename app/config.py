3d79fdca23b86fdf839be8010d5c4595adb0a768
agbq123 <abdulghanibadrquraishi@gmail.com>
2026-03-15 20:32:06 -0500

    Added Twilio placeholder configs

--- app/config.py
+++ app/config.py
@@ -3,3 +3,6 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///retention.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TWILIO_ACCOUNT_SID = "your_sid"
+    TWILIO_AUTH_TOKEN = "your_token"
+    TWILIO_PHONE = "+1234567890"
