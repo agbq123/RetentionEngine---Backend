0af3467c04a3a836bebea0a0e4c8e78dfe99fad3
agbq123 <abdulghanibadrquraishi@gmail.com>
2026-04-08 22:07:56 -0500

    Added getter for appointsments in churn algo

--- app/services/churn_service.py
+++ app/services/churn_service.py
@@ -1,2 +1,10 @@
 from datetime import datetime
+from ..models.appointment import Appointment
 
+def _get_client_appointments(client_id):
+    return (
+        Appointment.query
+        .filter(Appointment.client_id == client_id)
+        .order_by(Appointment.appointment_date.asc())
+        .all()
+    )
